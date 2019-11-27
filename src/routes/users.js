933f5a933b2d01189496d9d02227794a8d716e4b
JosueCortez-UCA <00402918@uca.edu.sv>
2019-11-27 11:52:47 -0600

    RM: nombres y rutas de NOTES quitadas

--- src/routes/users.js
+++ src/routes/users.js
@@ -41,7 +41,7 @@ router.get('/users/signin', (req, res) => {
 });
 
 router.post('/users/signin', passport.authenticate('local', {
-  successRedirect: '/notes',
+  successRedirect: '/reserv',
   failureRedirect: '/users/signin',
   failureFlash: true
 }));
