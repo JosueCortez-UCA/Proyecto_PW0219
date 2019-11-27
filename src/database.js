faa7a108d23b17a77bc6ca5b5ec65bb4ea07e82f
solis-g <00062418@uca.edu.sv>
2019-11-27 09:00:03 -0600

    ADD: profile screen

--- src/database.js
+++ src/database.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 mongoose.connect('mongodb+srv://admin:sashita@cluster0-9hjn6.mongodb.net/control-labs?retryWrites=true&w=majority', {
   useCreateIndex: true,
-  useNewUrlParser: true
+  useNewUrlParser: true, 
+    useUnifiedTopology:true
 })
   .then(db => console.log('DB is connected'))
   .catch(err => console.error(err));
