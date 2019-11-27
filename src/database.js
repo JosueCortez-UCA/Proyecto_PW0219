3238f4f5e133ef4231ce4023f81b4ffcd66a38d5
JosueCortez-UCA <00402918@uca.edu.sv>
2019-11-26 22:41:07 -0600

    ADD: conexion a atlas

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb+srv://admin:sashita@cluster0-lr4nu.mongodb.net/Control-labs?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://admin:sashita@cluster0-9hjn6.mongodb.net/test?retryWrites=true&w=majority', {
   useCreateIndex: true,
   useNewUrlParser: true
 })
